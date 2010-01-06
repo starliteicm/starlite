a669e6804a107395d0dc91b16918173ddc0e5668
jelliott <jelliott@i-tao.com>
2010-01-06 11:58:50 +0000

    days on contract

--- starlite-ui/src/main/java/com/itao/starlite/ui/actions/ReportsAction.java
+++ starlite-ui/src/main/java/com/itao/starlite/ui/actions/ReportsAction.java
@@ -1,12 +1,23 @@
 package com.itao.starlite.ui.actions;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.joda.time.DateMidnight;
 
 import com.google.inject.Inject;
 import com.itao.starlite.auth.User;
 import com.itao.starlite.auth.UserAware;
+import com.itao.starlite.model.Charter;
+import com.itao.starlite.model.CharterList;
+import com.itao.starlite.model.CrewDay;
 import com.itao.starlite.model.CrewMember;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -25,8 +36,24 @@ public class ReportsAction extends ActionSupport implements UserAware {
     
 	public List<CrewMember> crewMembers;
 	
+	@SuppressWarnings("unchecked")
+	public TreeMap charterMap;
+	
 	@Inject
 	private StarliteCoreManager manager;
+
+	public String dateFrom;
+	public String dateTo;
+	public SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+	public SimpleDateFormat dayformat = new SimpleDateFormat("dd");
+	public SimpleDateFormat monthformat = new SimpleDateFormat("MMMM");
+	public SimpleDateFormat yearformat = new SimpleDateFormat("yyyy");
+	public SimpleDateFormat fullformat = new SimpleDateFormat("dd MMMM yyyy");
+	public SimpleDateFormat mysqlFormat = new SimpleDateFormat("yyyy-MM-dd");
+	@SuppressWarnings("unchecked")
+	public LinkedHashMap<String,LinkedHashMap<String,TreeMap>> years;
+	public int daycount;
+	public String[] colors = new String[]{"6495ED","7B68EE","EEE8AA","90EE90","F4A460","CD5C5C","8FBC8F"};
 	
 	@Override
 	public String execute() throws Exception {
@@ -42,6 +69,227 @@ public class ReportsAction extends ActionSupport implements UserAware {
 			LOG.info(c.getPersonal().getFirstName());
 		}*/
 		return SUCCESS;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String days183() throws Exception {
+		
+		LOG.info("Running from "+dateFrom+" to "+dateTo);
+		
+		//Setup maps
+		charterMap = new TreeMap<String,TreeMap<String,TreeMap>>();
+		
+		//for each charter
+		CharterList charters = manager.getAllCharters();
+		
+	    Date start = df.parse(dateFrom);
+		Date end = df.parse(dateTo);
+		
+		
+		years  = new LinkedHashMap<String,LinkedHashMap<String,TreeMap>>();
+		
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(start);
+
+		while(cal.getTime().before(end)){
+			String year = yearformat.format(cal.getTime());
+			String month = monthformat.format(cal.getTime());
+			
+			if(years.containsKey(year)){
+				LinkedHashMap<String,TreeMap> months = years.get(year);	
+				if(months.containsKey(month)){
+					TreeMap days = months.get(month);
+					String day = dayformat.format(cal.getTime());
+					Map dayMap = new HashMap();
+					dayMap.put("day", cal.get(Calendar.DAY_OF_WEEK));
+					days.put(day,dayMap);
+					months.put(month, days);
+					years.put(year, months);
+					cal.add(Calendar.DAY_OF_MONTH, 1);
+				}
+				else{
+					TreeMap days = new TreeMap();
+					String day = dayformat.format(cal.getTime());
+					Map dayMap = new HashMap();
+					dayMap.put("day", cal.get(Calendar.DAY_OF_WEEK));
+					days.put(day,dayMap);
+					months.put(month, days);
+					years.put(year, months);
+					cal.add(Calendar.DAY_OF_MONTH, 1);
+				}
+			}
+			else{
+				LinkedHashMap<String,TreeMap> months = new LinkedHashMap<String,TreeMap>();
+				TreeMap days = new TreeMap();
+				String day = dayformat.format(cal.getTime());
+				TreeMap dayMap = new TreeMap();
+				dayMap.put("day", cal.get(Calendar.DAY_OF_WEEK));
+				days.put(day,dayMap);
+				months.put(month, days);
+				years.put(year, months);
+				cal.add(Calendar.DAY_OF_MONTH, 1);
+			}
+			daycount ++;
+		}
+
+		for(Charter chart : charters.charterList){
+			
+			//fromdate to todate get all crew days
+			List<CrewDay> crewDays = manager.getCrewDayByCharterBetween(chart.getId(),start, end); 
+			
+			TreeMap<String,TreeMap> aircraftMap = new TreeMap<String,TreeMap>();
+			
+			for(CrewDay day : crewDays){
+				
+				if(day.getAircraft() != null){
+				  //if they are assigned to an aircraft
+				  if(aircraftMap.containsKey(day.getAircraft().getRef())){
+					  TreeMap<String,TreeMap<String,TreeMap>> typeMap = aircraftMap.get(day.getAircraft().getRef());
+					  LOG.info(day.getCrewMember().getRole().getPosition()+" "+ day.getCrewMember().getId());
+					  if((""+day.getCrewMember().getRole().getPosition()).toUpperCase().indexOf("AME") != -1){
+						if(typeMap.containsKey("ZAME")){
+							TreeMap<String,TreeMap> groupMap = typeMap.get("ZAME");
+							TreeMap<String,TreeMap> crewMap = groupMap.get("crew");
+							TreeMap<String,Integer> totalMap = groupMap.get("total");
+							
+							if(crewMap.containsKey(day.getCrewMember().getPersonal().getFullName())){
+								TreeMap dayMap = crewMap.get(day.getCrewMember().getPersonal().getFullName());
+								dayMap.put(fullformat.format(day.getDate()), day);
+								crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+								groupMap.put("crew",crewMap);
+								
+								if(totalMap.containsKey(fullformat.format(day.getDate()))){
+									totalMap.put(fullformat.format(day.getDate()), totalMap.get(fullformat.format(day.getDate())) +1 );
+								}
+								else {
+									totalMap.put(fullformat.format(day.getDate()), 1 );									
+								}
+								
+								groupMap.put("total",totalMap);
+								typeMap.put("ZAME",groupMap);
+							}
+							else{
+								TreeMap dayMap = new TreeMap();
+								dayMap.put(fullformat.format(day.getDate()), day);
+								crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+								groupMap.put("crew",crewMap);
+								
+								if(totalMap.containsKey(fullformat.format(day.getDate()))){
+									totalMap.put(fullformat.format(day.getDate()), totalMap.get(fullformat.format(day.getDate())) +1 );
+								}
+								else {
+									totalMap.put(fullformat.format(day.getDate()), 1 );									
+								}
+								
+								groupMap.put("total",totalMap);
+								typeMap.put("ZAME",groupMap);
+							}
+						} 
+						else{
+							TreeMap groupMap = new TreeMap();
+							TreeMap crewMap = new TreeMap();
+							TreeMap totalMap = new TreeMap();
+							TreeMap dayMap = new TreeMap();
+							dayMap.put(fullformat.format(day.getDate()), day);
+							crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+							groupMap.put("crew",crewMap);
+							
+							totalMap.put(fullformat.format(day.getDate()), 1 );									
+							
+							groupMap.put("total",totalMap);
+							typeMap.put("ZAME",groupMap);
+						}
+					  }
+					  else{
+					    if(typeMap.containsKey("Pilot")){
+					    	TreeMap<String,TreeMap> groupMap = typeMap.get("Pilot");
+							TreeMap<String,TreeMap> crewMap = groupMap.get("crew");
+							TreeMap<String,Integer> totalMap = groupMap.get("total");
+					    	
+					    	if(crewMap.containsKey(day.getCrewMember().getPersonal().getFullName())){
+								TreeMap dayMap = crewMap.get(day.getCrewMember().getPersonal().getFullName());
+								dayMap.put(fullformat.format(day.getDate()), day);
+								crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+								groupMap.put("crew",crewMap);
+								
+								if(totalMap.containsKey(fullformat.format(day.getDate()))){
+									totalMap.put(fullformat.format(day.getDate()), totalMap.get(fullformat.format(day.getDate())) +1 );
+								}
+								else {
+									totalMap.put(fullformat.format(day.getDate()), 1 );									
+								}
+								
+								groupMap.put("total",totalMap);
+								typeMap.put("Pilot",groupMap);
+							}
+							else{
+								TreeMap dayMap = new TreeMap();
+								dayMap.put(fullformat.format(day.getDate()), day);
+								crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+								groupMap.put("crew",crewMap);
+								
+								if(totalMap.containsKey(fullformat.format(day.getDate()))){
+									totalMap.put(fullformat.format(day.getDate()), totalMap.get(fullformat.format(day.getDate())) +1 );
+								}
+								else {
+									totalMap.put(fullformat.format(day.getDate()), 1 );									
+								}
+								
+								groupMap.put("total",totalMap);
+								typeMap.put("Pilot",groupMap);
+							}
+					    	
+					    }
+					    else{
+					    	TreeMap groupMap = new TreeMap();
+							TreeMap crewMap = new TreeMap();
+							TreeMap totalMap = new TreeMap();
+							TreeMap dayMap = new TreeMap();
+							dayMap.put(fullformat.format(day.getDate()), day);
+							crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+							groupMap.put("crew",crewMap);
+							
+							totalMap.put(fullformat.format(day.getDate()), 1 );									
+							
+							groupMap.put("total",totalMap);
+							typeMap.put("Pilot",groupMap);
+					    }
+				      }
+				  }
+				  else{
+					  //aircraft not found add.
+					  TreeMap typeMap = new TreeMap();
+					  TreeMap groupMap = new TreeMap();
+					  TreeMap crewMap = new TreeMap();
+					  TreeMap totalMap = new TreeMap();
+					  TreeMap dayMap = new TreeMap();
+					  
+					  if((""+day.getCrewMember().getRole().getPosition()).toUpperCase().indexOf("AME") != -1){
+						  dayMap.put(fullformat.format(day.getDate()), day);
+						  crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+						  groupMap.put("crew",crewMap);
+						  totalMap.put(fullformat.format(day.getDate()), 1 );	
+						  groupMap.put("total",totalMap);
+						  typeMap.put("ZAME",groupMap);
+					  }
+					  else{
+						  dayMap.put(fullformat.format(day.getDate()), day);
+						  crewMap.put(day.getCrewMember().getPersonal().getFullName(),dayMap);
+						  groupMap.put("crew",crewMap);
+						  totalMap.put(fullformat.format(day.getDate()), 1 );	
+						  groupMap.put("total",totalMap);
+						  typeMap.put("Pilot",groupMap);
+					  }
+					  aircraftMap.put(day.getAircraft().getRef(), typeMap);
+				  }
+			    }	
+			}
+			
+			charterMap.put(chart.getCode(),aircraftMap);
+		}
+		LOG.info("\n"+charterMap);
+		return "days183";
 	}
 
 	public void setUser(User arg0) {
--- starlite-ui/src/main/webapp/WEB-INF/pages/reports.ftl
+++ starlite-ui/src/main/webapp/WEB-INF/pages/reports.ftl
@@ -1,4 +1,195 @@
-<ul>
+<#setting number_format = "######" />
+<#include "/starlite.ftl">
+<html>
+<head>
+  <@enableJQuery/>
+  <@enableDatePickers/>
+<style>
+table tr td {
+border: 1px solid black;
+padding:2px;
+}
+table tr th {
+border: 1px solid black;
+padding:2px;
+font-weight:bold;
+}
+table.jCalendar td {
+border:none;
+padding:3px 5px;
+font-weight:normal;
+}
+</style>
+</head>
+<body>
+
+<script>
+
+  function validateOnContract(){
+    var err = 0;
+    
+    $("#dateFromMsg").html("");
+    $("#dateToMsg").html("");
+    
+    if($("#dateFrom").val() == ""){
+      err = 1;
+      $("#dateFromMsg").html("Required");
+    }
+    
+    if($("#dateTo").val() == ""){
+      err = 1;
+      $("#dateToMsg").html("Required");
+    }
+    
+    if(err == 0){
+      document.forms.oncontract.submit();
+    }
+    return false;
+  }
+  
+</script>
+
+<#if charterMap?exists >
+
+<li style="width:200px;height:30px;float:left;"><A href="reports.action" style="width:300px;height:20px;" >Back</A></li>
+<br/><br/>
+
+<#list charterMap.keySet()! as chartKey>
+  <#assign chart=charterMap.get(chartKey) />
+  <#if chart.size() != 0 >
+  
+    <#list chart.keySet()! as aircraftKey>
+      <#assign aircraft=chart.get(aircraftKey) />
+  
+      <table> 
+  
+       <#assign count=0 />
+  
+       <tr>
+       <th style="width:150px;"><div style="width:150px;">${chartKey}</div></th>
+       <#list years.keySet()! as year>
+         <#assign months= years.get(year) />
+         <#list months.keySet()! as month>
+           <#assign color=colors[count % 5] />
+           <#assign count=count+1 />
+           <#assign days=months.get(month) />
+           <td style="background-color:#${color};text-align:center;font-weight:bold;" colspan="${days.size()!}">${month}</td>
+         </#list>
+       </#list>
+       <td>&nbsp;</td>
+       <td>&nbsp;</td>
+       </tr>
+       
+       <tr>
+       <th style="width:150px;"><div style="width:150px;">${aircraftKey}</div></th>
+       <#list years.keySet()! as year>
+         <#assign months= years.get(year) />
+         <#list months.keySet()! as month>
+           <#assign days= months.get(month) />
+           <#list days.keySet()! as day>
+             <td>${day}</td>
+           </#list>
+         </#list>
+       </#list>
+       <td>Total</td>
+       <td>Excess</td>
+       </tr>
+  
+  
+      <#list aircraft.keySet()! as typeKey>
+        <#assign type=aircraft.get(typeKey) />
+        <#if typeKey=="ZAME">
+          <#assign typeColor="orange"/>
+        <#else>
+          <#assign typeColor="silver"/>
+        </#if>
+        
+        
+        <#list type.crew.keySet()! as crewKey>
+          <#assign crew=type.crew.get(crewKey) />
+          <#assign totalW=0 />
+          <#assign totalT=0 />
+          
+          <!--<tr>
+          <td style="width:150px;background-color:${typeColor};"><div style="width:150px;">${crewKey}</div></td>
+          <td colspan="${daycount}">${crew}</td>
+          </tr>-->
+          
+          <tr>
+          <td style="width:150px;background-color:${typeColor};"><div style="width:150px;">${crewKey}</div></td>
+          <#list years.keySet()! as year>
+            <#assign months= years.get(year) />
+            <#list months.keySet()! as month>
+              <#assign days= months.get(month) />
+              <#list days.keySet()! as day>
+                <#assign cd= days.get(day) />
+                <#assign date= ""+day+" "+month+" "+year />
+                <#if crew.get(date)?exists >
+                <#assign crewDay=crew.get(date) />
+                
+                <#if crewDay.activity == "W">
+                <#assign totalW=totalW+1 />
+                <td title="${date}"  <#if cd.day==1> style="border-right:2px solid black;" </#if>  > ${crewDay.activity!} </td>
+                <#elseif crewDay.activity == "T">
+                <#assign totalT=totalT+1 />
+                <td title="${date}"  <#if cd.day==1> style="border-right:2px solid black;" </#if>  > ${crewDay.activity!} </td>
+                <#else>
+                <td title="${date}"  <#if cd.day==1> style="border-right:2px solid black;" </#if>  > </td>
+                </#if>
+                
+                <#else>
+                <td title="${date}"  <#if cd.day==1> style="border-right:2px solid black;" </#if>  > </td>
+                </#if>
+              </#list>
+            </#list>
+          </#list>
+          
+          <td colspan="2" style="border:2px solid black">${totalW}W, ${totalT}T</td>
+    
+          </tr>
+        </#list>
+        
+        <tr style="background-color:${typeColor};">
+        <td style="width:150px;background-color:${typeColor};"><div style="width:150px;">&nbsp;</div></td>
+        <#assign over=0 />
+        <#assign totaltype=0 />
+        <#list years.keySet()! as year>
+            <#assign months= years.get(year) />
+            <#list months.keySet()! as month>
+              <#assign days= months.get(month) />
+              <#list days.keySet()! as day>
+                <#assign cd= days.get(day) />
+                <#assign total= type.total />
+                <#assign date= ""+day+" "+month+" "+year />
+                <#if total.get(date)?exists >
+                
+                <#assign totalday=total.get(date) />
+                <#if totalday gt 2>
+                <#assign over=over + (totalday - 2) />
+                </#if>
+                <#assign totaltype=totaltype+totalday />
+                
+                <td title="${date}"  <#if cd.day==1> style="border-right:2px solid black;" </#if>  > ${totalday} </td>
+                <#else>
+                <td title="${date}"  <#if cd.day==1> style="border-right:2px solid black;" </#if>  > </td>
+                </#if>
+              </#list>
+            </#list>
+          </#list>
+          <td>${totaltype}</td>
+          <td>${over}</td>
+        </tr>
+      </#list>
+      
+      </table>
+      <br/>
+    </#list>
+  </#if>
+</#list>
+
+
+<#else>
+  <ul>
 	<li style="width:200px;height:30px;"><a href="script.action?scriptName=aircraftCharterMatrix.groovy&month=${month}&year=${year?c}">Aircraft/Charter Matrix</a>
 	<li style="width:200px;height:30px;"><a href="script.action?scriptName=certificate.groovy">Certificates</a>
 	<li style="width:200px;height:30px;"><a href="script.action?scriptName=crewMemberProfiles.groovy">Crew Member Profiles</a>
@@ -10,7 +201,7 @@
 	<li style="width:200px;height:30px;"><a href="script.action?scriptName=crewPayments.groovy&month=${month}&year=${year?c}">Crew Payments</a>
 	<li style="width:200px;height:30px;"><a href="script.action?scriptName=crewPaymentAnalysis.groovy&month=${month}&year=${year?c}">Crew Payment Analysis</a>
 	</#if>
-</ul>
+  </ul>
 
 <form name="crewDoc" id="crewDoc" action="script.action">
     <input type="hidden" name="scriptName" value="crewDocumentAnalysis.groovy" />
@@ -29,8 +220,20 @@
 <form name="profile" id="profile" action="crewMember!profile.action">
 <li style="width:200px;height:30px;float:left;"><A href="#" style="width:300px;height:20px;" onclick="document.forms.profile.submit();" >View Profile</A></li>
 <select style="float:left" name="id">
-<#list crewMembers as crew>
+<#list crewMembers! as crew>
 <option value="${crew.code?if_exists}" > ${crew.personal.fullName?if_exists}
 </#list>
 </select>
 </form>
+<br/><br/>
+<form name="oncontract" id="oncontract" action="reports!days183.action">
+<li style="width:200px;height:30px;float:left;"><A href="#" style="width:300px;height:20px;" onclick="validateOnContract();" >View On Contract</A></li>
+  <div class="fm-opt">
+            <span style="color:red">*</span><input id="dateFrom" onfocus="this.blur();" name="dateFrom" type="text" class="date-pick" value=""/><span style="color:red" id="dateFromMsg"></span>
+            <span style="color:red">*</span><input id="dateTo"   onfocus="this.blur();" name="dateTo"   type="text" class="date-pick" value=""/><span style="color:red" id="dateToMsg"></span>
+  </div>
+</form>
+</#if>
+<div style="height:120px;">&nbsp;</div>
+</body>
+</html>
